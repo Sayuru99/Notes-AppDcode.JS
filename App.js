848ac47bd604bf7cdd5ec62678954c427c9cde87
Sayuru De Alwis <75390567+Sayuru99@users.noreply.github.com>
2021-12-16 01:25:08 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import NotesView from "./NotesView.js";
-import NotesAPI from "./NotesAPI.js";
+import NotesView from "NotesView.js";
+import NotesAPI from "NotesAPI.js";
 
 export default class App {
     constructor(root) {
